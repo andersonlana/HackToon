6f8de1a1d5cb8d29dc973876f95a9e614e3dd36b
andersonlana <anderson-lana@hotmail.com>
2024-11-23 15:46:22 -0300

    incluído o script.js

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -51,6 +51,7 @@
         </footer>
 
         <script scr="/js/app.js"></script>
+        <script src="/js/script.js"></script>
         <link rel="stylesheet" href="/css/style.css">
     </body>
 </html>
