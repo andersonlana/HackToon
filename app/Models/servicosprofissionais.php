4b5417f782cbeef4167cb82c22dc39d1ed9a3f47
andersonlana <anderson-lana@hotmail.com>
2024-11-24 10:31:02 -0300

    commit

--- app/Models/servicosprofissionais.php
+++ app/Models/servicosprofissionais.php
@@ -1,19 +1,12 @@
-<?php
+<?php 
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class servicosprofissionais extends Model
+class ServicosProfissionais extends Model
 {
     use HasFactory;
-
-    protected $dates = ['date'];
-    protected $table = 'servicosprofissionais'; // Nome da tabela
-
-    public function user() {
-        return $this->belongsTo('App\Models\User');
-
-    }
+    protected $table = "servicosprofissionais";
 }
